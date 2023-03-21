c747481c77d89dc909b36dc73e4989c6669c7bdf
andreapdr <andrea.pedrotti@isti.cnr.it>
2023-03-21 12:14:16 +0100

    update to integrate into benchmark code for UniVL

--- model.py
+++ model.py
@@ -1,4 +1,11 @@
+import os
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
 import sys
+
+sys.path.append(current_dir)
+
 import torch as th
 import torchvision.models as models
 from videocnn.models import resnext, s3dg
@@ -12,15 +19,16 @@ class GlobalAvgPool(nn.Module):
     def forward(self, x):
         return th.mean(x, dim=[-2, -1])
 
+
 def init_weight(model, state_dict, should_omit="s3dg."):
     old_keys = []
     new_keys = []
     for key in state_dict.keys():
         new_key = None
-        if 'gamma' in key:
-            new_key = key.replace('gamma', 'weight')
-        if 'beta' in key:
-            new_key = key.replace('beta', 'bias')
+        if "gamma" in key:
+            new_key = key.replace("gamma", "weight")
+        if "beta" in key:
+            new_key = key.replace("beta", "bias")
         if new_key:
             old_keys.append(key)
             new_keys.append(new_key)
@@ -31,7 +39,7 @@ def init_weight(model, state_dict, should_omit="s3dg."):
     unexpected_keys = []
     error_msgs = []
     # copy state_dict so _load_from_state_dict can modify it
-    metadata = getattr(state_dict, '_metadata', None)
+    metadata = getattr(state_dict, "_metadata", None)
     state_dict = state_dict.copy()
     if metadata is not None:
         state_dict._metadata = metadata
@@ -42,67 +50,74 @@ def init_weight(model, state_dict, should_omit="s3dg."):
         for key in state_dict.keys():
             if key.find(should_omit) == 0:
                 old_keys.append(key)
-                new_key = key[len(should_omit):]
+                new_key = key[len(should_omit) :]
                 new_keys.append(new_key)
         for old_key, new_key in zip(old_keys, new_keys):
             state_dict[new_key] = state_dict.pop(old_key)
 
-    def load(module, prefix=''):
+    def load(module, prefix=""):
         local_metadata = {} if metadata is None else metadata.get(prefix[:-1], {})
         module._load_from_state_dict(
-            state_dict, prefix, local_metadata, True, missing_keys, unexpected_keys, error_msgs)
+            state_dict,
+            prefix,
+            local_metadata,
+            True,
+            missing_keys,
+            unexpected_keys,
+            error_msgs,
+        )
         for name, child in module._modules.items():
             if child is not None:
-                load(child, prefix + name + '.')
+                load(child, prefix + name + ".")
 
-    load(model, prefix='')
+    load(model, prefix="")
 
-    if len(missing_keys) > 0:
-        print("Weights of {} not initialized from pretrained model: {}".format(model.__class__.__name__, "\n   " + "\n   ".join(missing_keys)))
-    if len(unexpected_keys) > 0:
-        print("Weights from pretrained model not used in {}: {}".format(model.__class__.__name__, "\n   " + "\n   ".join(unexpected_keys)))
-    if len(error_msgs) > 0:
-        print("Weights from pretrained model cause errors in {}: {}".format(model.__class__.__name__, "\n   " + "\n   ".join(error_msgs)))
+    # if len(missing_keys) > 0:
+    #     print("Weights of {} not initialized from pretrained model: {}".format(model.__class__.__name__, "\n   " + "\n   ".join(missing_keys)))
+    # if len(unexpected_keys) > 0:
+    #     print("Weights from pretrained model not used in {}: {}".format(model.__class__.__name__, "\n   " + "\n   ".join(unexpected_keys)))
+    # if len(error_msgs) > 0:
+    #     print("Weights from pretrained model cause errors in {}: {}".format(model.__class__.__name__, "\n   " + "\n   ".join(error_msgs)))
 
     return model
 
+
+def get_s3dg_model(s3dg_path, device="cpu"):
+    model = s3dg.S3D(last_fc=False)
+    model = model.to(device)
+    model_data = th.load(s3dg_path)
+    model = init_weight(model, model_data)
+    model.eval()
+    return model
+
+
 def get_model(args):
-    assert args.type in ['2d', '3d', 's3dg']
-    if args.type == '2d':
-        print('Loading 2D-ResNet-152 ...')
+    assert args.type in ["2d", "3d", "s3dg"]
+    if args.type == "2d":
+        print("Loading 2D-ResNet-152 ...")
         model = models.resnet152(pretrained=True)
         model = nn.Sequential(*list(model.children())[:-2], GlobalAvgPool())
         model = model.cuda()
-    elif args.type == '3d':
-        print('Loading 3D-ResneXt-101 ...')
+    elif args.type == "3d":
+        print("Loading 3D-ResneXt-101 ...")
         model = resnext.resnet101(
             num_classes=400,
-            shortcut_type='B',
+            shortcut_type="B",
             cardinality=32,
             sample_size=112,
             sample_duration=16,
-            last_fc=False)
+            last_fc=False,
+        )
         model = model.cuda()
         model_data = th.load(args.resnext101_model_path)
         model.load_state_dict(model_data)
-    elif args.type == 's3dg':
-        print('Loading S3DG ...')
+    elif args.type == "s3dg":
+        print("Loading S3DG ...")
         model = s3dg.S3D(last_fc=False)
         model = model.cuda()
         model_data = th.load(args.s3d_model_path)
         model = init_weight(model, model_data)
 
-
     model.eval()
-    print('loaded')
+    print("loaded")
     return model
-
-if __name__ == "__main__":
-    model = resnext.resnet101(
-        num_classes=400,
-        shortcut_type='B',
-        cardinality=32,
-        sample_size=112,
-        sample_duration=16,
-        last_fc=False)
-    print(model)
--- video_loader.py
+++ video_loader.py
@@ -10,11 +10,11 @@ class VideoLoader(Dataset):
     """Pytorch video loader."""
 
     def __init__(
-            self,
-            csv,
-            framerate=1,
-            size=112,
-            centercrop=False,
+        self,
+        csv,
+        framerate=1,
+        size=112,
+        centercrop=False,
     ):
         """
         Args:
@@ -29,10 +29,12 @@ class VideoLoader(Dataset):
 
     def _get_video_dim(self, video_path):
         probe = ffmpeg.probe(video_path)
-        video_stream = next((stream for stream in probe['streams']
-                             if stream['codec_type'] == 'video'), None)
-        width = int(video_stream['width'])
-        height = int(video_stream['height'])
+        video_stream = next(
+            (stream for stream in probe["streams"] if stream["codec_type"] == "video"),
+            None,
+        )
+        width = int(video_stream["width"])
+        height = int(video_stream["height"])
         return height, width
 
     def _get_output_dim(self, h, w):
@@ -44,37 +46,34 @@ class VideoLoader(Dataset):
             return self.size, int(w * self.size / h)
 
     def __getitem__(self, idx):
-        video_path = self.csv['video_path'].values[idx]
-        output_file = self.csv['feature_path'].values[idx]
+        video_path = self.csv["video_path"].values[idx]
+        output_file = self.csv["feature_path"].values[idx]
         video = th.zeros(1)
 
         if not (os.path.isfile(output_file)) and os.path.isfile(video_path):
-            print('Decoding video: {}'.format(video_path))
+            print("Decoding video: {}".format(video_path))
             try:
                 h, w = self._get_video_dim(video_path)
 
                 height, width = self._get_output_dim(h, w)
                 cmd = (
-                    ffmpeg
-                        .input(video_path)
-                        .filter('fps', fps=self.framerate)
-                        .filter('scale', width, height)
+                    ffmpeg.input(video_path)
+                    .filter("fps", fps=self.framerate)
+                    .filter("scale", width, height)
                 )
                 if self.centercrop:
                     x = int((width - self.size) / 2.0)
                     y = int((height - self.size) / 2.0)
                     cmd = cmd.crop(x, y, self.size, self.size)
-                out, _ = (
-                    cmd.output('pipe:', format='rawvideo', pix_fmt='rgb24')
-                        .run(capture_stdout=True, quiet=True)
+                out, _ = cmd.output("pipe:", format="rawvideo", pix_fmt="rgb24").run(
+                    capture_stdout=True, quiet=True
                 )
                 if self.centercrop and isinstance(self.size, int):
                     height, width = self.size, self.size
                 video = np.frombuffer(out, np.uint8).reshape([-1, height, width, 3])
-                video = th.from_numpy(video.astype('float32'))
+                video = th.from_numpy(video.astype("float32"))
                 video = video.permute(0, 3, 1, 2)
             except:
-                print('ffprobe failed at: {}'.format(video_path))
+                print("ffprobe failed at: {}".format(video_path))
 
-        return {'video': video, 'input': video_path, 'output': output_file}
-
+        return {"video": video, "input": video_path, "output": output_file}
